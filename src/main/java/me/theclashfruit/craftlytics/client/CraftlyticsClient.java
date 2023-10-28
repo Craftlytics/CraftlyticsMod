f82d6dddd71ab67e0120a914aec7c1febf5bff66
TheClashFruit <clash.fruit123@gmail.com>
2023-10-28 14:23:50 +0200

    feat: create project

---
+++ src/main/java/me/theclashfruit/craftlytics/client/CraftlyticsClient.java
@@ -0,0 +1,13 @@
+package me.theclashfruit.craftlytics.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftlyticsClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
