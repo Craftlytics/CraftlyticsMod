054cdcf5eb38dd1a2d375a610e4f2ca131dc448f
TheClashFruit <clash.fruit123@gmail.com>
2023-10-29 23:30:10 +0100

    feat: the funny

--- src/main/java/me/theclashfruit/craftlytics/Craftlytics.java
+++ src/main/java/me/theclashfruit/craftlytics/Craftlytics.java
@@ -4,13 +4,25 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import me.theclashfruit.craftlytics.config.CraftlyticsConfig;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
 
 public class Craftlytics implements ModInitializer {
-    /**
-     * Runs the mod initializer.
-     */
+    public static final String MOD_ID = "craftlytics";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static final String[] randomMessage = {
+        "waht?!", // me
+        "say gex", // mya in crss server
+        "I am a typical Windows 85 user. Nothings strange here!" // worldwidepixel in modrinth server
+    };
+
     @Override
     public void onInitialize() {
+        // the funny
+        LOGGER.info(randomMessage[new Random().nextInt(randomMessage.length)]);
 
         // Register Config
         AutoConfig.register(CraftlyticsConfig.class, JanksonConfigSerializer::new);
--- src/main/java/me/theclashfruit/craftlytics/config/CraftlyticsConfig.java
+++ src/main/java/me/theclashfruit/craftlytics/config/CraftlyticsConfig.java
@@ -5,5 +5,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = "craftlytics")
 public class CraftlyticsConfig implements ConfigData {
-    boolean test = true;
+    boolean optOut = false;
+    String excludedMods = "examplemodid,anothermod";
+    String apiUrl = "https://craftlytics.theclashfruit.me/api";
 }
